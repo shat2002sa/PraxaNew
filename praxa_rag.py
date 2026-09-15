109f3f945fdbafb4c7e37e919d163033ebd6cca1
shat2002sa <257138854+shat2002sa@users.noreply.github.com>
2026-09-15 21:18:17 +0300

    uncommented the lines 78-83 and ran the code

--- praxa_rag.py
+++ praxa_rag.py
@@ -75,12 +75,12 @@ if __name__ == "__main__":
 #    result = chain.invoke("What is Ryan Calais Cameron's most recent play?")
 #    print(result.content)
 
-#    result = chain_with_sources.invoke("What Broadway shows have had more than 10,000 performances?")
-#    print("The docs used in this answer:")
-#    print("\n".join(doc.metadata.__repr__() for doc in result["context_docs"]))
-#    print("-----")
-#    print("The answer:")
-#    print(result["answer"].content)
+    result = chain_with_sources.invoke("What Broadway shows have had more than 10,000 performances?")
+    print("The docs used in this answer:")
+    print("\n".join(doc.metadata.__repr__() for doc in result["context_docs"]))
+    print("-----")
+    print("The answer:")
+    print(result["answer"].content)
 
 #    print(answer_and_sources("What is Ryan Calais Cameron's most recent play?"))
 
